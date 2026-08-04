45a8f75813285cab77e28cc0f8d2ead77f226b92
rajdeepsinh jadeja <rajdeepsinhj494@gmail.com>
2026-08-04 12:39:09 +0530

    Add files via upload

---
+++ lab_manual/unit_2/unit_2_5.py
@@ -0,0 +1,19 @@
+for i in range(1,11):
+    if(i==6):
+        break
+    print(i)
+    
+print('===========================================')
+
+for i in range(1,11):
+    if(i==6):
+        continue
+    print(i)
+
+print('===========================================')
+
+def message():
+    pass
+
+for i in range(1,11):
+    message()
---
+++ lab_manual/unit_2/unit_2_6.py
@@ -0,0 +1,16 @@
+'''Write a program to iterate over lists strings and 
+dictionaries using loops. '''
+
+
+lst = ['apple','banana','graps']
+dict1 = {
+    1 : 'apple',
+    2 : 'banana',
+    3 : 'graps'
+}
+
+for i in lst:
+    print([i])
+
+for i in dict1.items():
+    print(i)
