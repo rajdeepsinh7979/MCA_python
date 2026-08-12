539216b426ad4628dacf48631dc19d6de07382d9
rajdeepsinh jadeja <rajdeepsinhj494@gmail.com>
2026-08-12 13:13:55 +0530

    unit 3 4th and 5th added.

---
+++ lab_manual/unit_3/unit_3_4.py
@@ -0,0 +1,6 @@
+'''4. Write a program to generate random numbers 
+using random module.'''
+
+import random as rn
+
+print(rn.random())
---
+++ lab_manual/unit_3/unit_3_5.py
@@ -0,0 +1,7 @@
+'''5. Write a program to display current date and 
+time using date time module. 
+'''
+
+from datetime import datetime
+
+print(datetime.now())
